8338f60ae3520860943e19a1714890c85dff8e87
Greg <mayhem2423@hotmail.com>
2018-11-13 21:36:42 -0500

    Merge branch 'setup' of https://github.com/MalachiLabs360/silverback into setup
    # Conflicts:
    #	application/views/elements_default/banners/mileposts.php
    #	application/views/pages/products/edifyassess/main.php
    #	application/views/pages/products/mileposts/how_it_works.php
    #	application/views/pages/products/mileposts/main.php
    #	application/views/pages/products/teachervitae/main.php

--- application/views/pages/products/teachervitae/features.php
+++ application/views/pages/products/teachervitae/features.php
@@ -4,19 +4,17 @@
             <div class="col-lg-6">
                 <h2 class="iq-tw-6 iq-mb-25">Teacher Vitae by Silverback</h2>
                 <p class="iq-font-15">Revolutionize the way your team looks at instructional effectiveness.</p>
-                <ul class="iq-mt-40 iq-list">
+                <ul class="iq-mt-40 iq-list float-left">
                     <li class="iq-tw-7"><i class="ion-wand iq-mr-20 iq-font-blue iq-font-30"></i><span class="iq-font-black">Works on Any Device</span></li>
                     <li class="iq-tw-7"><i class="ion-levels iq-mr-20 iq-font-blue iq-font-30"></i><span class="iq-font-black">Offers Multiple Evaluation Measures</span></li>
                     <li class="iq-tw-7"><i class="ion-help-buoy iq-mr-20 iq-font-blue iq-font-30"></i><span class="iq-font-black">Includes Portable Professional Portfolios</span></li>
                     <li class="iq-tw-7"><i class="ion-asterisk iq-mr-20 iq-font-blue iq-font-30"></i><span class="iq-font-black">Individualized Learning Plans</span></li>
-                </ul>
-                <ul class="iq-mt-40 iq-list">
                     <li class="iq-tw-7"><i class="ion-wand iq-mr-20 iq-font-blue iq-font-30"></i><span class="iq-font-black">Launches Ad Hoc Forms</span></li>
                     <li class="iq-tw-7"><i class="ion-levels iq-mr-20 iq-font-blue iq-font-30"></i><span class="iq-font-black">Quick Status Dashboard Views</span></li>
+                </ul>
+                <ul class="iq-mt-40 iq-list float-sm-right float-left">
                     <li class="iq-tw-7"><i class="ion-help-buoy iq-mr-20 iq-font-blue iq-font-30"></i><span class="iq-font-black">Centralized Location for all Facets of Staff Evaluation</span></li>
                     <li class="iq-tw-7"><i class="ion-asterisk iq-mr-20 iq-font-blue iq-font-30"></i><span class="iq-font-black">Complete Walkthroughs, Summative Evaluations,Observations etc.</span></li>
-                </ul>
-                <ul class="iq-mt-40 iq-list">
                     <li class="iq-tw-7"><i class="ion-wand iq-mr-20 iq-font-blue iq-font-30"></i><span class="iq-font-black">Scheduling, Bulk Scheduling, Scoring, Sharing and e-Signing</span></li>
                     <li class="iq-tw-7"><i class="ion-levels iq-mr-20 iq-font-blue iq-font-30"></i><span class="iq-font-black">Customizable Forms, Templates and Rubrics WithWide Variety of Field Types</span></li>
                     <li class="iq-tw-7"><i class="ion-help-buoy iq-mr-20 iq-font-blue iq-font-30"></i><span class="iq-font-black">Unique Tagging System Tying Every Observationand Note to the State and/or District Summative Evaluation</span></li>
--- application/views/pages/products/teachervitae/main.php
+++ application/views/pages/products/teachervitae/main.php
@@ -2,8 +2,8 @@
 <?php $this->view('pages/products/teachervitae/features',$params); ?>
 <?php $this->view('pages/products/teachervitae/educator_growth',$params); ?>
 <?php $this->view('pages/products/teachervitae/faq',$params); ?>
-<?php $this->view('pages/products/teachervitae/student',$params); ?>
-<?php $this->view('pages/products/teachervitae/study',$params); ?>
+<?php /*$this->view('pages/products/teachervitae/student',$params); */?><!--
+--><?php /*$this->view('pages/products/teachervitae/study',$params); */?>
 <style>
 .navbar-light .navbar-nav .nav-link {
     color: rgba(0,0,0,.5) !important;
