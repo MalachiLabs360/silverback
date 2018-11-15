fb05dfae00921617f05ac26b8521dfadf7fa2815
Conrad Sage <conrad.sage@smart-edge.com>
2018-11-15 00:23:46 -0500

    michelle list

--- application/views/elements_default/banners/edifyassess.php
+++ application/views/elements_default/banners/edifyassess.php
@@ -4,7 +4,7 @@
             <div class="row">
                 <div class="col-lg-6 home-top-text">
                     <h1 class="text-uppercase iq-font-blue iq-tw-3"><b class="iq-tw-7" style="color: #ffffff">Edify Assess</b></h1>
-                    <h3 class="text-uppercase iq-font-blue iq-tw-3" style="color: #54cbf5">PERSONALIZE THE LEARNING JOURNEY</h3>
+                    <h3 class="text-uppercase iq-font-blue iq-tw-3 header_sub_text">PERSONALIZE THE LEARNING JOURNEY</h3>
                     <p class="iq-font-white iq-pt-15 iq-mb-40">Mileposts provides a holistic view of every student so educators and administrators can make more informed decisions, help each learner achieve personalized success, and keep parents up to date with their child’s progress</p>
                 </div>
                 <div class="col-lg-6">
--- application/views/elements_default/banners/mileposts.php
+++ application/views/elements_default/banners/mileposts.php
@@ -1,10 +1,10 @@
 <section id="iq-home" class="iq-banner overview-block-pt iq-bg-over iq-over-blue-90 jarallax" data-jarallax-video="mp4:/assets/front/video/blurred_people_walking.mp4">
     <div class="container">
-        <div class="banner-text home-top-text">
+        <div class="banner-text">
             <div class="row">
-                <div class="col-lg-6">
+                <div class="col-lg-6 home-top-text">
                     <h1 class="text-uppercase iq-font-blue iq-tw-3"><b class="iq-tw-7" style="color: #ffffff">MILEPOSTS</b></h1>
-                    <h3 class="text-uppercase iq-font-blue iq-tw-3" style="color: #54cbf5">PERSONALIZE THE LEARNING JOURNEY</h3>
+                    <h3 class="text-uppercase iq-font-blue iq-tw-3 header_sub_text">PERSONALIZE THE LEARNING JOURNEY</h3>
                     <p class="iq-font-white iq-pt-15 iq-mb-40">Mileposts provides a holistic view of every student so educators and administrators can make more informed decisions, help each learner achieve personalized success, and keep parents up to date with their child’s progress</p>
                 </div>
                 <div class="col-lg-6">
