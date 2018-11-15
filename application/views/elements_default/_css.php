347661b191cd3bbbafd78e984f2147fc1fb2ce21
Conrad Sage <conrad.sage@smart-edge.com>
2018-11-15 00:37:13 -0500

    michelle list

--- application/views/elements_default/_css.php
+++ application/views/elements_default/_css.php
@@ -1,26 +1,13 @@
-<!-- Favicon -->
-<link rel="shortcut icon" href="/assets/front/images/favicon.ico" />
-<!-- Google Fonts -->
 <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700,800&amp;Raleway:300,400,500,600,700,800,900" rel="stylesheet">
-<!-- Bootstrap CSS -->
 <link rel="stylesheet" href="/assets/front/css/bootstrap.min.css">
-<!-- owl-carousel -->
 <link rel="stylesheet" href="/assets/front/css/owl-carousel/owl.carousel.css" />
-<!-- Font Awesome -->
 <link rel="stylesheet" href="/assets/front/css/font-awesome.css" />
-<!-- Magnific Popup -->
 <link rel="stylesheet" href="/assets/front/css/magnific-popup/magnific-popup.css" />
-<!-- media element player -->
 <link href="/assets/front/css/mediaelementplayer.min.css" rel="stylesheet" type="text/css" />
-<!-- Animate -->
 <link rel="stylesheet" href="/assets/front/css/animate.css" />
-<!-- Ionicons -->
 <link rel="stylesheet" href="/assets/front/css/ionicons.min.css">
-<!-- Style -->
 <link rel="stylesheet" href="/assets/front/css/style.css">
-<!-- Responsive -->
 <link rel="stylesheet" href="/assets/front/css/responsive.css">
-<!-- custom style -->
 <link rel="stylesheet" href="/assets/front/css/custom.css" />
 
 <?php 
