5df76e4b3e6d25a4a0c1e1e98971eca464bc9400
Conrad Sage <conrad.sage@smart-edge.com>
2018-11-15 00:30:00 -0500

    michelle list

--- application/views/elements_default/banners/news.php
+++ application/views/elements_default/banners/news.php
@@ -3,9 +3,9 @@
     <div class="container">
         <div class="banner-text">
             <div class="row">
-                <div class="col-lg-6" style="background-color: rgba(0, 0, 0, 0.3)">
+                <div class="col-lg-6 readable">
                     <h1 class="iq-font-white text-uppercase iq-tw-5"><b>News from <span class="iq-font-green">Silverback Learning</span> </b></h1>
-                    <p class="iq-font-white iq-pt-15 iq-mb-40">We are not an education technology company. We are an educational improvement and enhancement company that believes we can radically change the way educators do business in the classroom.</p>
+                    <p class="iq-font-white iq-pt-15">We are not an education technology company. We are an educational improvement and enhancement company that believes we can radically change the way educators do business in the classroom.</p>
                 </div>
                 <div class="col-lg-6">
                 </div>
