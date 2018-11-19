449fd19c883e660952703ba5213148b3348aeaae
Greg <mayhem2423@hotmail.com>
2018-11-19 05:36:57 -0500

    Customized Forms for checkboxes to help with submit functionality in custom.js file and to fixe both boxes turning red if only one is being filled out.

--- application/views/pages/contact/form.php
+++ application/views/pages/contact/form.php
@@ -19,38 +19,38 @@
         <form id="contact_sales" method="post">
             <div class="contact-form">
                 <div class="section-field">
-                    <input class="require" id="contact_name" type="text" placeholder="Full Name*" name="name">
+                    <input class="require_sales" id="contact_name" type="text" placeholder="Full Name*" name="name">
                 </div>
                 <div class="section-field">
-                    <input class="require" id="contact_district" type="text" placeholder="District or Company*" name="district">
+                    <input class="require_sales" id="contact_district" type="text" placeholder="District or Company*" name="district">
                 </div>
                 <div class="section-field">
-                    <input class="require" id="contact_email" type="email" placeholder="Email*" name="email">
+                    <input class="require_sales" id="contact_email" type="email" placeholder="Email*" name="email">
                 </div>
                 <div class="section-field">
-                    <input class="require" id="position_git ptitle" type="text" placeholder="Position Title - Superintendent, Teacher, Assessment Coordinator*" name="position">
+                    <input class="require_sales" id="position_git ptitle" type="text" placeholder="Position Title - Superintendent, Teacher, Assessment Coordinator*" name="position">
                 </div>
                 <div class="section-field">
-                    <input class="require" id="phone_number" type="tel" placeholder="Phone Number - (555)-555-5555*" name="number">
+                    <input class="require_sales" id="phone_number" type="tel" placeholder="Phone Number - (555)-555-5555*" name="number">
                 </div>
                 <div>
                     <p><b>I am interested in learning more about:</b></p>
-                    <div class="form-group">
+                    <div id="interest_box_form" class="form-group">
                         <div class="form-check">
-                            <input type="checkbox" class="form-check-input" id="mileposts_box">
+                            <input type="checkbox" class="form-check-input" id="mileposts_box" name="interest_box">
                             <label class="font-weight-normal text-dark" for="mileposts_button">Mileposts (Personalized Learning)</label>
                         </div>
                         <div class="form-check">
-                            <input type="checkbox" class="form-check-input" id="teacher_box">
+                            <input type="checkbox" class="form-check-input" id="teacher_box" name="interest_box">
                             <label class="font-weight-normal text-dark" for="teacher_box">Teacher Vitae (Instructional Effectiveness)</label>
                         </div>
                         <div class="form-check">
-                            <input type="checkbox" class="form-check-input" id="edify_box">
+                            <input type="checkbox" class="form-check-input" id="edify_box" name="interest_box">
                             <label class="font-weight-normal text-dark" for="edify_box">EdifyAssess (Formative Assessments)</label>
                         </div>
                     </div>
                 </div>
-                <div class="required selection-field mb-3">
+                <div id="hear_about_selection" class="required selection-field mb-3" data-toggle="collapse">
                     <p><b>How did you hear about us?</b></p>
                     <select id="inputStateSales" class="form-control">
                         <option>Conference</option>
@@ -62,14 +62,14 @@
                         <option>Referral</option>
                     </select>
                 </div>
-                <div class="section-field">
-                    <input class="require" id="referral_info" type="text" placeholder="If this was a referral, please tell us who referred you.*" name="referral">
+                <div class="section-field d-md-none d-lg-block" data-toggle="collapse">
+                    <input class="collapse" id="referral_info" type="text" placeholder="Please tell us who referred you.*" name="referral">
                 </div>
                 <div class="section-field">
-                    <input class="require" id="how_can_we_help" type="text" placeholder="How can we help you?*" name="howcan">
+                    <input class="require_sales" id="how_can_we_help" type="text" placeholder="How can we help you?*" name="howcan">
                 </div>
                 <button id="submit" name="submit" type="submit" value="Send" class="button iq-mt-15">Send Message</button>
-                <div class="alert alert-success alert-dismissible fade show" role="alert" id="success_sales">
+                <div class="alert alert-success alert-dismissible fade show mt-3" role="alert" id="success_sales">
                     <strong>Thank You, Your message has been received.</strong>.
                     <button type="button" class="close" data-dismiss="alert" aria-label="Close">
                         <span aria-hidden="true">&times;</span>
@@ -90,16 +90,16 @@
         <form id="contact_support" method="post">
             <div class="contact-form">
                 <div class="section-field">
-                    <input class="require" id="contact_name" type="text" placeholder="Full Name*" name="name">
+                    <input class="require_support" id="contact_name" type="text" placeholder="Full Name*" name="name">
                 </div>
                 <div class="section-field">
-                    <input class="require" id="contact_location" type="text" placeholder="Location (State)*" name="location">
+                    <input class="require_support" id="contact_location" type="text" placeholder="Location (State)*" name="location">
                 </div>
                 <div class="section-field">
-                    <input class="require" id="contact_district" type="text" placeholder="District*" name="district">
+                    <input class="require_support" id="contact_district" type="text" placeholder="District*" name="district">
                 </div>
                 <div class="section-field">
-                    <input class="require" id="contact_email" type="email" placeholder="Email*" name="email">
+                    <input class="require_support" id="contact_email" type="email" placeholder="Email*" name="email">
                 </div>
                 <div class="selection-field mb-3">
                     <p><b>Product:</b></p>
@@ -115,7 +115,7 @@
                     <textarea id="contact_message" class="input-message require" placeholder="*Please provide as many details as possible about the problem you are experiencing." rows="5" name="message"></textarea>
                 </div>
                 <button id="submit" name="submit" type="submit" value="Send" class="button iq-mt-15">Send Message</button>
-                <div class="alert alert-success alert-dismissible fade show" role="alert" id="success_support">
+                <div class="alert alert-success alert-dismissible fade show  mt-3" role="alert" id="success_support">
                     <strong>Thank You, Your message has been received.</strong>.
                     <button type="button" class="close" data-dismiss="alert" aria-label="Close">
                         <span aria-hidden="true">&times;</span>
